0e4c831b3334734c22019fc1e7daeb62c38339c3
Anton Lyhin <dotnet@lyhin.com>
2021-07-02 17:16:57 +0300

    Bai.Media Watermark POC (https://localhost:13001/api/Image endpoint);

--- Bai.Media.Web/Controllers/ImageController.cs
+++ Bai.Media.Web/Controllers/ImageController.cs
@@ -1,6 +1,13 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 using Bai.General.DAL.Abstractions.Repositories;
 using Bai.Media.DAL.Models;
+using Bai.Media.Web.Services.MediaServices;
+using ImageMagick;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bai.Media.Web.Controllers
@@ -17,5 +24,87 @@ namespace Bai.Media.Web.Controllers
         //[HttpPost]
         //public virtual async Task<ActionResult> Post([FromBody] TModel entity) =>
         //    Ok(await _repository.AddEntity(entity, true));
+
+        [HttpGet]
+        public async Task<ActionResult> WatermarkImage() => GetWatermarkImage();
+
+        private ActionResult WatermarkMagicImage()
+        {
+            using var webClient = new WebClient();
+            string imageUrl = "https://localhost:13001/bai.media.staticfiles/predefined/images/default.jpg"; // default.jpg // calendar-application.jpg
+            string watermarkUrl = "https://localhost:13001/bai.media.staticfiles/predefined/watermarks/default.png";
+
+            var imageBytes = webClient.DownloadData(imageUrl);
+            var watermarkBytes = webClient.DownloadData(watermarkUrl);
+
+            var imageStream = GetStreamFromByteArray(imageBytes);
+            var watermarkStream = GetStreamFromByteArray(watermarkBytes);
+
+            var processedImage = new WatermarkService().AddWatermark(imageStream, watermarkStream);
+
+            return File(processedImage.ToByteArray(), "image/jpeg");
+        }
+
+        private ActionResult GetWatermarkImage(bool defaultSize = true)
+        {
+            using var webClient = new WebClient();
+            string imageUrl = "https://localhost:13001/bai.media.staticfiles/predefined/images/calendar-application.jpg";
+            string watermarkUrl = "https://localhost:13001/bai.media.staticfiles/predefined/watermarks/default.png";
+
+            var imageBytes = webClient.DownloadData(imageUrl);
+            var watermarkBytes = webClient.DownloadData(watermarkUrl);
+
+            var image = GetImageFromByteArray(imageBytes);
+            using var watermark = GetImageFromByteArray(watermarkBytes);
+
+            if (defaultSize)
+            {
+                image = new WatermarkService().ResizeImage(image);
+            }
+
+            var processedImage = new WatermarkService().AddWatermark2(image, watermark);
+            var processedImageBytes = ImageToByteArray(processedImage);
+            return File(processedImageBytes, "image/jpeg");
+        }
+
+        //private string GetMimeTypeFromFileName(string fileName)
+        //{
+        //    new FileExtensionContentTypeProvider().TryGetContentType(fileName, out var contentType);
+        //    return contentType ?? "application/octet-stream";
+        //}
+
+        private Stream GetStreamFromByteArray(byte[] byteArray)
+        {
+            var ms = new MemoryStream(byteArray, 0, byteArray.Length);
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms;
+        }
+
+        private Image GetImageFromByteArray(byte[] byteArray)
+        {
+            var ms = new MemoryStream(byteArray, 0, byteArray.Length);
+            ms.Seek(0, SeekOrigin.Begin);
+            return Image.FromStream(ms);
+        }
+
+        private byte[] ImageToByteArray(Image imageIn)
+        {
+            try
+            {
+                var converter = new ImageConverter();
+                return (byte[])converter.ConvertTo(imageIn, typeof(byte[]));
+
+                using (var ms = new MemoryStream())
+                {
+                    imageIn.Save(ms, ImageFormat.Png); // , imageIn.RawFormat
+                    return ms.ToArray();
+                }
+            }
+            catch (Exception e)
+            {
+                var x = e;
+                return null;
+            }
+        }
     }
 }
---
+++ Bai.Media.Web/Services/MediaServices/WatermarkService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using ImageMagick;
+
+namespace Bai.Media.Web.Services.MediaServices
+{
+    public class WatermarkService
+    {
+        public MagickImage AddWatermark(Stream imageStream, Stream watermarkStream)
+        {
+            using var image = new MagickImage(imageStream);
+            using var watermark = new MagickImage(watermarkStream);
+
+            try
+            {
+                var size = new MagickGeometry(100, 100);
+                size.IgnoreAspectRatio = true;
+                image.Resize(size);
+
+                image.Composite(watermark, watermark.Width, watermark.Height, CompositeOperator.Over);
+                image.Write(watermarkStream);
+            }
+            catch (Exception e)
+            {
+                // Output file write error --- out of disk space? `' @ error/jpeg.c/JPEGErrorHandler/346
+
+                // WriteBlob Failed `' @ error/png.c/MagickPNGErrorHandler/1715
+                // https://github.com/dlemstra/Magick.NET/issues/562
+                var x = e.Message;
+                throw;
+            }
+
+            return image;
+        }
+
+        public Image AddWatermark2(Image image, Image watermark)
+        {
+            using (Graphics imageGraphics = Graphics.FromImage(image))
+            using (var watermarkBrush = new TextureBrush(watermark))
+            {
+                int x = (image.Width - 30 - watermark.Width);
+                int y = (image.Height - 20 - watermark.Height);
+
+                // https://stackoverflow.com/questions/4113900/c-sharp-add-watermark-to-the-photo-by-special-way
+                watermarkBrush.TranslateTransform(x, y);
+                imageGraphics.FillRectangle(watermarkBrush, new Rectangle(new Point(x, y), new Size(watermark.Width + 1, watermark.Height)));
+                return image;
+            }
+        }
+
+        public Bitmap ResizeImage(Image image, int width = 845, int height = 475) // int width = 961, int height = 541
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
