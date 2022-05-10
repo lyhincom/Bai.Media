6539093df7864710a3a3281d0c4b458582e84a2f
Anton Lyhin <dotnet@lyhin.com>
2022-05-10 19:48:53 +0300

    Ready for first deploy;

--- Bai.Media.Web/Startup.cs
+++ Bai.Media.Web/Startup.cs
@@ -1,14 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using Bai.Domain.Settings;
 using Bai.Domain.Settings.Getters;
 using Bai.General.ApplicationBuilder;
 using Bai.General.DAL.Abstractions.Repositories;
 using Bai.General.DAL.Repositories;
-using Bai.General.Environments;
-using Bai.General.Environments.Enums;
 //using Bai.General.JwtToken;
+using Bai.General.Logs;
 using Bai.General.Swagger;
 using Bai.Media.DAL.Contexts;
 using Bai.Media.DAL.Models;
@@ -28,6 +26,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Serilog;
 
 namespace Bai.Media.Web
 {
@@ -80,21 +79,25 @@ namespace Bai.Media.Web
                 sql.MigrationsAssembly("Bai.Media.Migrations");
                 sql.MigrationsHistoryTable("__EFMigrationsHistory", "dbo");
             }));
+
+            services.AddLogs();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MediaDbContext context)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MediaDbContext context, ILogger logger)
         {
+            logger.Warning($"Bai.Media environment {env.EnvironmentName}...");
+
             app.UseDomainDeveloperExceptionPage(env);
             app.UseDomainHsts();
             //if (DomainEnvironment.Parse(env.EnvironmentName) == EnvironmentEnum.Local)
-            {
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bai.Media.Web v1"));
-            }
+            //}
 
             app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
+            //app.UseAuthentication();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
