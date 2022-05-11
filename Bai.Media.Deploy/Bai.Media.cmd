cc6268992b0ea34f83f72bccbc93a482028b202b
Anton Lyhin <dotnet@lyhin.com>
2022-05-11 21:24:50 +0300

    Deploy Script;

---
+++ Bai.Media.Deploy/Bai.Media.cmd
@@ -0,0 +1,1 @@
+msbuild "D:\Bai.By\Bai.Services\Bai.Media\Bai.Media.Web\Bai.Media.Web.csproj" /p:DeployOnBuild=true /p:PublishProfile="D:\Bai.By\Bai.Services\Bai.Media\Bai.Media.Web\Properties\PublishProfiles\media-qa.bai.by - Web Deploy.pubxml" /p:Password= /p:VisualStudioVersion=17.1
